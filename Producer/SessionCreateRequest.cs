e1aa9bebe8082b319c1755e18c2a9004a1d68bec
SynysterNght <xxpedroatxx@gmail.com>
2020-05-05 19:59:20 -0400

    Before leaving

---
+++ Producer/SessionCreateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Producer
+{
+    class SessionCreateRequest
+    {
+        public string SessionId { get; set; }
+        public int NumberOfMessagesPerSession { get; set; }
+        public string TestRunId { get; set; }
+        public int ConsumerWorkTime { get; set; }
+    }
+}
---
+++ Producer/SessionMessagesCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Producer
+{
+    class SessionMessagesCreateRequest
+    {
+        public string SessionId { get; set; }
+        public int MessageId { get; set; }
+    }
+}
